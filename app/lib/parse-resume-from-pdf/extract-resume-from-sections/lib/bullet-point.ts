57fc77069abcd7daf054b7ee7bfd031cfb05ffca
eraykeskinmac <eraykeskinmac@gmail.com>
2024-09-06 19:19:39 +0300

    refactor: Update bullet point symbols and improve bullet point line indexing logic

--- app/lib/parse-resume-from-pdf/extract-resume-from-sections/lib/bullet-point.ts
+++ app/lib/parse-resume-from-pdf/extract-resume-from-sections/lib/bullet-point.ts
@@ -1,1 +1,39 @@
-export const BULLET_POINTS = [];
+import { Lines } from "../../types";
+
+export const BULLET_POINTS = [
+  "⋅",
+  "∙",
+  "🞄",
+  "•",
+  "⦁",
+  "⚫︎",
+  "●",
+  "⬤",
+  "⚬",
+  "○",
+];
+
+const getFirstBulletPointLineIdx = (lines: Lines): number | undefined => {
+  for (let i = 0; i < lines.length; i++) {
+    for (let item of lines[i]) {
+      if (BULLET_POINTS.some((bullet) => item.text.includes(bullet))) {
+        return i;
+      }
+    }
+  }
+  return undefined;
+};
+
+export const getDescriptionLineIdx = (lines: Lines): number | undefined => {
+  let idx = getFirstBulletPointLineIdx(lines);
+
+  if (idx === undefined) {
+    for (let i = 0; i < lines.length; i++) {
+      for (let item of lines[i]) {
+        if (item.text.includes("•")) {
+          return i;
+        }
+      }
+    }
+  }
+};

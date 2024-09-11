91710f7b117f6439bd5db00ee4e7efb3ead1b351
eraykeskinmac <eraykeskinmac@gmail.com>
2024-09-12 01:50:14 +0300

    refactor: Improve work experience extraction logic and scoring system

--- app/lib/parse-resume-from-pdf/extract-work-experince.ts
+++ app/lib/parse-resume-from-pdf/extract-work-experince.ts
@@ -1,7 +1,18 @@
 import { ResumeWorkExperience } from "../redux/types";
+import { hasNumber } from "./extract-resume-from-sections/extract-profile";
+import {
+  getBulletPointsFromLines,
+  getDescriptionLineIdx,
+} from "./extract-resume-from-sections/lib/bullet-point";
+import {
+  DATE_FEATURE_SETS,
+  getHasText,
+} from "./extract-resume-from-sections/lib/common-feature";
+import { getTextWithHighestFeatureScore } from "./extract-resume-from-sections/lib/feature-scoring-system";
 import { getSectionLinesByKeywords } from "./extract-resume-from-sections/lib/get-section-lines";
 import { divideSectionIntoSubsection } from "./extract-resume-from-sections/lib/subsection";
-import { ResumeSectionToLines } from "./types";
+import { isBold } from "./group-lines-into-sections";
+import { FeatureSet, ResumeSectionToLines, TextItem } from "./types";
 
 const WORK_EXPERIENCE_KEYWORDS_LOWERCASE = [
   "work",
@@ -21,6 +32,19 @@ const JOB_TITLES = [
   "CTO",
 ];
 
+const hasJobTitle = (item: TextItem) =>
+  JOB_TITLES.some((jobTitle) =>
+    item.text.split(/\s/).some((word) => word === jobTitle)
+  );
+
+const hasMoreThan5Words = (item: TextItem) => item.text.split(/\s/).length > 5;
+
+const JOB_TITLE_FEATURE_LIST: FeatureSet[] = [
+  [hasJobTitle, 4],
+  [hasNumber, -4],
+  [hasMoreThan5Words, -2],
+];
+
 export const extractWorkExperince = (sections: ResumeSectionToLines) => {
   const workExperince: ResumeWorkExperience[] = [];
   const workExperinceScores = [];
@@ -32,6 +56,45 @@ export const extractWorkExperince = (sections: ResumeSectionToLines) => {
   const subsections = divideSectionIntoSubsection(lines);
 
   for (const subsectionLines of subsections) {
-    const descriptionsLinesIdx = 
+    const descriptionsLinesIdx = getDescriptionLineIdx(subsectionLines) ?? 2;
+
+    const subsectionInfoTextItem = subsectionLines
+      .slice(0, descriptionsLinesIdx)
+      .flat();
+
+    const [date, dateScores] = getTextWithHighestFeatureScore(
+      subsectionInfoTextItem,
+      DATE_FEATURE_SETS
+    );
+
+    const [jobTitle, jobTitleScores] = getTextWithHighestFeatureScore(
+      subsectionInfoTextItem,
+      JOB_TITLE_FEATURE_LIST
+    );
+
+    const COMPANY_FEATURE_SET: FeatureSet[] = [
+      [isBold, 2],
+      [getHasText(date), -4],
+      [getHasText(jobTitle), -4],
+    ];
+
+    const [company, companyScores] = getTextWithHighestFeatureScore(
+      subsectionInfoTextItem,
+      COMPANY_FEATURE_SET,
+      false
+    );
+
+    const subsectionDescriptionLines =
+      subsectionLines.slice(descriptionsLinesIdx);
+    const descriptions = getBulletPointsFromLines(subsectionDescriptionLines);
+
+    workExperince.push({ company, jobTitle, date, descriptions });
+    workExperinceScores.push({
+      companyScores,
+      jobTitleScores,
+      dateScores,
+    });
   }
+
+  return { workExperince, workExperinceScores };
 };
